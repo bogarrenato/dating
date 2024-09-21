7a1cbb008dcdd8723a46d21f95b49d9d6059791c
Bogár Renátó <bogarrenato@gmail.com>
2024-09-21 21:02:48 +0200

    Add cors

--- API/Program.cs
+++ API/Program.cs
@@ -12,10 +12,12 @@ builder.Services.AddDbContext<DataContext>(options =>
     // appsettings.development.json - bol olvassa ki
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Middleware start
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 // Configure the HTTP request pipeline.
 // Maps controllers
 app.MapControllers();
