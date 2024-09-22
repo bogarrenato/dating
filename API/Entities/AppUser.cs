16e52a33eb62e04d75b3387e76f333d6f6312ef7
Bogár Renátó <bogarrenato@gmail.com>
2024-09-22 08:35:43 +0200

    eof s4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -9,4 +9,6 @@ public class AppUser
     public int Id { get; set; }
     // Cannot create appuser without username
     public required string UserName { get; set; }
+    public required byte[] PasswordHash { get; set; }
+    public required byte[] PasswordSalt { get; set; }
 }
---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,26 @@
+﻿using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions;
+
+public static class ApplicationServiceExtensions
+{
+    // Extend this
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddControllers();
+        // Register our data context
+        services.AddDbContext<DataContext>(options =>
+        {
+            // appsettings.development.json - bol olvassa ki
+            options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+        });
+        services.AddCors();
+        // AddScoped -  Created once by client request
+        services.AddScoped<ITokenService, TokenService>();
+
+        return services;
+    }
+}
